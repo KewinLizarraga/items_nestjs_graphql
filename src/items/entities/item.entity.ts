4292023f18ae8e9c2225087c18a9623e90605ac3
kLizarraga <kewin392@gmail.com>
2024-03-19 00:22:59 -0500

    add auth

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,5 +1,13 @@
 import { ObjectType, Field, ID, Float } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from '../../users/entities/user.entity';
 
 @Entity({ name: 'items' })
 @ObjectType()
@@ -12,14 +20,19 @@ export class Item {
   @Field(() => String)
   name: string;
 
-  @Column()
-  @Field(() => Float)
-  quantity: number;
+  // @Column()
+  // @Field(() => Float)
+  // quantity: number;
 
   @Column({ nullable: true })
   @Field(() => String, { nullable: true })
   quantityUnits?: string; // g, ml, kg, tsp
 
   // stores
+
   // user
+  @ManyToOne(() => User, (user) => user.items, { nullable: false, lazy: true })
+  @Index('userId-index')
+  @Field(() => User)
+  user: User;
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class ItemsService {
@@ -11,33 +12,48 @@ export class ItemsService {
     @InjectRepository(Item) private readonly itemsRepository: Repository<Item>,
   ) {}
 
-  async create(createItemInput: CreateItemInput): Promise<Item> {
-    const newItem = this.itemsRepository.create(createItemInput);
+  async create(createItemInput: CreateItemInput, user: User): Promise<Item> {
+    const newItem = this.itemsRepository.create({ ...createItemInput, user });
     return await this.itemsRepository.save(newItem);
   }
 
-  async findAll(): Promise<Item[]> {
+  async findAll(user: User): Promise<Item[]> {
     // TODO - filtrar, paginacion, por usuario ...
-    return await this.itemsRepository.find();
+    const items = await this.itemsRepository.find({
+      where: { user },
+      relations: { user: true },
+    });
+    return items;
   }
 
-  async findOne(id: string): Promise<Item> {
-    const item = await this.itemsRepository.findOne({ where: { id } });
+  async findOne(id: string, user: User): Promise<Item> {
+    const item = await this.itemsRepository.findOne({
+      where: { id, user },
+      relations: { user: true },
+    });
     if (!item) throw new NotFoundException('Item not found');
     return item;
   }
 
-  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+  async update(
+    id: string,
+    updateItemInput: UpdateItemInput,
+    user: User,
+  ): Promise<Item> {
+    await this.findOne(id, user);
     // const item = await this.findOne(id);   // Esto o la siguiente linea, funciona igual
     const item = await this.itemsRepository.preload(updateItemInput);
     if (!item) throw new NotFoundException('Item not found');
     return this.itemsRepository.save(item);
   }
 
-  async remove(id: string): Promise<Item> {
-    // TODO - soft delete, integridad referencial
-    const item = await this.findOne(id);
+  async remove(id: string, user: User): Promise<Item> {
+    const item = await this.findOne(id, user);
     await this.itemsRepository.remove(item);
     return { ...item, id };
+  }
+
+  async itemCountByUser(user: User): Promise<number> {
+    return this.itemsRepository.count({ where: { user: { id: user.id } } });
   }
 }
